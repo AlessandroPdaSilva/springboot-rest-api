799fbebc111cf944fcff3b4771cd29c35446aaf6
Alessandro <alessandropds55@gmail.com>
2022-08-29 12:07:45 -0300

    Criando consulta com banco de dados com Spring Data

--- springboot-rest-api-sample/src/main/java/application/controllers/GreetingsController.java
+++ springboot-rest-api-sample/src/main/java/application/controllers/GreetingsController.java
@@ -1,20 +1,47 @@
 package application.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import application.model.Usuario;
+import application.repository.UsuarioRepository;
  
 @RestController
 public class GreetingsController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
   
     @RequestMapping(value = "/{url}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String greetingText(@PathVariable String url) {
     	
+    	Usuario u = new Usuario();
+    	
+    	u.setIdade(20);
+    	u.setNome("Guilherme");
+    	usuarioRepository.save(u);
+    	
         return "Ola mundo: " + url + "!";
+    }
+    
+    @GetMapping(value = "listausuarios")// API buscar usuarios
+    @ResponseBody
+    public ResponseEntity<List<Usuario>> listarUsuarios(){
+    	
+    	List<Usuario> listaUsuario = usuarioRepository.findAll();
+    	
+    	return new ResponseEntity<List<Usuario>>(listaUsuario, HttpStatus.OK);
     }
     
 }
---
+++ springboot-rest-api-sample/src/main/java/application/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package application.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import application.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+}
